f3694d145fefc6cf4fb0039005e67adbbb398e53
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2021-10-24 16:21:34 +0800

    added hero icons

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,7 +12,7 @@ export default NextAuth({
   providers: [
     Providers.Credentials({
       credentials: {
-        username: {
+        username: { 
           type: 'text',
         },
         password: {
